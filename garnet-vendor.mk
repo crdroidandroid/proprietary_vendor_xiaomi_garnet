769367bd011a7a70b13507aaf409ff3c72b952d8
UnmoveD <unmoved2121@gmail.com>
2025-02-27 01:25:57 +0100

    Revert "garnet: audio: Nuke dirac and misoundfx effect"
    This reverts commit cefbf2a71011d89e47f55b0a938a53b2fe07afb1.

--- garnet-vendor.mk
+++ garnet-vendor.mk
@@ -1733,6 +1733,7 @@ PRODUCT_PACKAGES += \
     sensors.touch.detect \
     sensors.ultrasoundproximity \
     libasphere \
+    libmisoundfx \
     libshoebox \
     vendor.display.color@1.0 \
     vendor.display.color@1.1 \
