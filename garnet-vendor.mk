e8d5e6ad93c0bc0ba6db5c8cc81b616a28af49cd
Jotanh <jotanh@jotanic.xyz>
2026-09-05 11:26:57 -0300

    garnet: Restore eUICC mirilhook from b62ffb5
    Restore EUICC Files

--- garnet-vendor.mk
+++ garnet-vendor.mk
@@ -1841,6 +1841,7 @@ PRODUCT_PACKAGES += \
     uimgbamanagerlibrary \
     uimservicelibrary \
     com.android.hotwordenrollment.common.util \
+    mirilhook \
     qcrilhook \
     vendor.qti.hardware.alarm-V1.0-java \
     vendor.qti.ims.connection-V1.0-java \
